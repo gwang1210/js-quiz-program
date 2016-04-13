eea6d14dda9f4c247a5c908daadb4dd63b689b27
gwang1210 <gary.wang@wustl.edu>
2016-04-13 18:02:18 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,12 +2,50 @@
 var correct = 0;
 
 // prompt
-var answer1 = prompt("ask a question...");
+var answer1 = prompt("what's heavier, a feather or a brick?...");
+var answer2 = prompt("What color is a green house");
+var answer3 = prompt("if I had 2 apples, how many apples do I have?");
+var answer4 = prompt("did Sally really sell seashells at the seashore?");
+var answer5 = prompt("How much wood could a woodchuck chuck if a woodchuck couldn't chuck wood?");
 
 // if statement
-if ( answer1.toUpperCase() === 'MAGIC' ) {
+if ( answer1.toUpperCase() === 'BRICK' ) {
+ correct += 1;
+}
+if ( answer1.toUpperCase() === 'A BRICK' ) {
+ correct += 1;
+}
+if ( answer2.toUpperCase() === 'GREEN' ) {
+ correct += 1;
+}
+if ( answer3.toUpperCase() === '2' ) {
+ correct += 1;
+}
+if ( answer4.toUpperCase() === 'YES' ) {
+ correct += 1;
+}
+if ( answer5.toUpperCase() === '0' ) {
+ correct += 1;
+}
+if ( answer5.toUpperCase() === 'NONE' ) {
  correct += 1;
 }
 
 // output results
-document.write("<p>write something to page...</p>");
+document.write("<p>" + answer1 + "</p>");
+document.write("<p>" + answer2 + "</p>");
+document.write("<p>" + answer3 + "</p>");
+document.write("<p>" + answer4 + "</p>");
+document.write("<p>" + answer5 + "</p>");
+
+
+if ( correct === '5' ) {
+document.write("<p>CONGRATULATIONS. YOU GOT 5 RIGHT.</p>");
+}
+if ( correct === '4' ) {
+document.write("<p>CONGRATULATIONS. YOU GOT 4 RIGHT.</p>");
+}
+if ( correct === '5' ) {
+document.write("<p>CONGRATULATIONS. YOU GOT 5 RIGHT.</p>");
+}
+
